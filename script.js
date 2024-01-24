c417edeefc5a04e740a263313d658cd8307176f3
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-24 18:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function calculate(){
+	let num1=prompt("Enter the first number");
+	let num2=prompt("Enter the second number");
+	num1*=2;
+	let result=num1+num2;
+	alert(`The final result is: ${result}`);
+}
+// calculate();
